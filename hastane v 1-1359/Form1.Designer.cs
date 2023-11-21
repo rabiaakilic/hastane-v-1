162ec50e508a43f65eb974b36d53ee120d17ff5f
rabiaakilic <147382992+rabiaakilic@users.noreply.github.com>
2023-11-21 11:59:13 +0300

    Add files via upload

---
+++ hastane v 1-1359/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace hastane_v_1_1359
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.rbKronikHastalıkVar = new System.Windows.Forms.RadioButton();
+            this.rbKronikHastalıkYok = new System.Windows.Forms.RadioButton();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.cmbKan = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SOYAD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TC NO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "TEL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "KRONİK HASTALIK";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "KAN  GURUBU ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 364);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "DOĞUM TARİHİ";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(100, 54);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(134, 20);
+            this.txtAd.TabIndex = 7;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(116, 99);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(134, 20);
+            this.txtSoyad.TabIndex = 8;
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(116, 150);
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(134, 20);
+            this.txtTcNo.TabIndex = 9;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(87, 204);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(134, 20);
+            this.txtTel.TabIndex = 10;
+            // 
+            // rbKronikHastalıkVar
+            // 
+            this.rbKronikHastalıkVar.AutoSize = true;
+            this.rbKronikHastalıkVar.Location = new System.Drawing.Point(165, 253);
+            this.rbKronikHastalıkVar.Name = "rbKronikHastalıkVar";
+            this.rbKronikHastalıkVar.Size = new System.Drawing.Size(150, 17);
+            this.rbKronikHastalıkVar.TabIndex = 11;
+            this.rbKronikHastalıkVar.TabStop = true;
+            this.rbKronikHastalıkVar.Text = "KRONİK HASTALIĞI VAR";
+            this.rbKronikHastalıkVar.UseVisualStyleBackColor = true;
+            // 
+            // rbKronikHastalıkYok
+            // 
+            this.rbKronikHastalıkYok.AutoSize = true;
+            this.rbKronikHastalıkYok.Location = new System.Drawing.Point(165, 276);
+            this.rbKronikHastalıkYok.Name = "rbKronikHastalıkYok";
+            this.rbKronikHastalıkYok.Size = new System.Drawing.Size(150, 17);
+            this.rbKronikHastalıkYok.TabIndex = 12;
+            this.rbKronikHastalıkYok.TabStop = true;
+            this.rbKronikHastalıkYok.Text = "KRONİK HASTALIĞI YOK";
+            this.rbKronikHastalıkYok.UseVisualStyleBackColor = true;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(147, 358);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(200, 20);
+            this.dtpTarih.TabIndex = 14;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(423, 189);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(98, 48);
+            this.btnEkle.TabIndex = 15;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // cmbKan
+            // 
+            this.cmbKan.FormattingEnabled = true;
+            this.cmbKan.Items.AddRange(new object[] {
+            "0-",
+            "0+",
+            "A+",
+            "A-",
+            "B+",
+            "B-",
+            "AB+",
+            "AB-"});
+            this.cmbKan.Location = new System.Drawing.Point(165, 313);
+            this.cmbKan.Name = "cmbKan";
+            this.cmbKan.Size = new System.Drawing.Size(121, 21);
+            this.cmbKan.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 450);
+            this.Controls.Add(this.cmbKan);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dtpTarih);
+            this.Controls.Add(this.rbKronikHastalıkYok);
+            this.Controls.Add(this.rbKronikHastalıkVar);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtTcNo);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.RadioButton rbKronikHastalıkVar;
+        private System.Windows.Forms.RadioButton rbKronikHastalıkYok;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ComboBox cmbKan;
+    }
+}
+
---
+++ hastane v 1-1359/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hastane_v_1_1359
+{
+    public partial class Form1 : Form
+    {
+        Hasta hasta1= new Hasta();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+
+            
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+
+           hasta1 = new Hasta();
+
+            hasta1.ad = txtAd.Text;
+            hasta1.soyad = txtSoyad.Text;
+            hasta1.tcno = txtTcNo.Text; 
+            hasta1.tel = txtTel.Text;
+            hasta1.kronikHastalik = rbKronikHastalıkVar.Checked;
+            hasta1.kangurubu = cmbKan.Text;
+            hasta1.dtarih = dtpTarih.Value;
+
+        }  
+    }
+}
